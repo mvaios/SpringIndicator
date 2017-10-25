28504e1fcf81f51c82ca1f89ff72129afb2d177e
Vahram Mirzoyan <vahram@tickaroo.com>
2017-10-25 15:39:42 +0200

    Crush fix

--- SpringIndicator/RefreshIndicator.swift
+++ SpringIndicator/RefreshIndicator.swift
@@ -71,7 +71,7 @@ public class RefreshIndicator: UIControl {
         super.willMove(toSuperview: newSuperview)
 
         targetView = newSuperview as? UIScrollView
-        initialInsetTop = targetView!.contentInset.top
+        initialInsetTop = targetView?.contentInset.top ?? 0
     }
 
     open override func didMoveToSuperview() {
